c0d446e5482f0b9064a6c34e619631098ab27b98
_chandrashekher_ <cpsaw999041@gmail.com>
2023-11-11 00:20:32 +0530

    Rupees into USD

--- Firstassign/src/Q5.java
+++ Firstassign/src/Q5.java
@@ -5,7 +5,7 @@ public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter currency in rupees : ");
-        int Ir = sc.nextInt();
+        float Ir = sc.nextInt();
         float USD = Ir / 83;
         System.out.print("Rupees in dollars : " +USD);
 
