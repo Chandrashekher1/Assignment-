e735bd49bc7d71f919852e271fdacda72c0539aa
_chandrashekher_ <cpsaw999041@gmail.com>
2023-11-11 22:54:59 +0530

    area code

---
+++ Firstassign/src/BasicArea.java
@@ -0,0 +1,59 @@
+// 1. Area Of Circle Java Program
+
+//import java.util.Scanner;
+//public class BasicArea {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter radius of circle : ");
+//        int r = sc.nextInt();
+//        double area = (3.14*r*r);
+//        System.out.print(area);
+//
+//    }
+//}
+
+
+// 2. Area Of Triangle
+//import java.util.Scanner;
+//public class BasicArea {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter base of Triangle : ");
+//        int base = sc.nextInt();
+//        System.out.print("Enter height of triangle : ");
+//        int h = sc.nextInt();
+//        int area = (base*h)/2;
+//        System.out.print("Area of triangle is :" +area);
+//
+//    }
+//}
+
+
+// 3 .Area Of Rectangle Program
+//import java.util.Scanner;
+//public class BasicArea {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter Length of rectangle : ");
+//        int L = sc.nextInt();
+//        System.out.print("Enter Breath of rectangle : ");
+//        int B = sc.nextInt();
+//        int area = L*B;
+//        System.out.print("Area of rectangle of is " +area);
+//
+//    }
+//}
+
+// 4. Area Of Parallelogram
+import java.util.Scanner;
+public class BasicArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base of parallelogram :");
+        int b = sc.nextInt();
+        System.out.print("enter height of parallelogram :");
+        int h = sc.nextInt();
+        int area = b*h;
+        System.out.print("Area of paralellogram is :" +area);
+    }
+}
